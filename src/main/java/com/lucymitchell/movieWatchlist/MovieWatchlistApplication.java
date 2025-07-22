7e87a053317da9349eb17e366719fd1ad40c6a7e
lucy-e-mitchell <lucymitchell574@gmail.com>
2025-07-22 10:52:45 +0100

    Initial commit

---
+++ src/main/java/com/lucymitchell/movieWatchlist/MovieWatchlistApplication.java
@@ -0,0 +1,13 @@
+package com.lucymitchell.movieWatchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWatchlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucymitchell/movieWatchlist/MovieWatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucymitchell.movieWatchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieWatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
